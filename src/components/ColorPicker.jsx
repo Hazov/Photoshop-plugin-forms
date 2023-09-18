4ba2e27d19726bd59c1d1e8e8ce197885794786d
hazov <nonaliud@mail.ru>
2023-09-18 20:42:31 +0300

    Первая выпадашка

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -10,31 +10,59 @@ const app = photoshop.app;
 const storage = uxp.storage;
 const imaging = photoshop.imaging;
 const formats = storage.formats
+let currentFormFolder = ['allFiles','forms']
+let isInit = false;
 
 export const ColorPicker = () => {
-    let [forms, setForms] = useState([]);
-    getAllOfEntity('forms').then(resolve => {
-        setForms(resolve);
-    })
-    // setForms(getAllOfEntity('forms'));
+    let [formCategory, setFormCategory] = useState({title: '', categoryItems: []});
+    let [filteredFormItems, setFilteredFormItems] = useState([]);
+    init();
+
+    function init(){
+        if(!isInit){
+            getAllOfEntity('forms').then(resolve => setForms(resolve));
+            fetchFormCategory().then(resolve => setFormCategory(resolve));
+            isInit = true
+        }
+    }
     const createNewLayerInApp = async (evt) =>  {
         let activeDocument = app.activeDocument;
         execute(() => activeDocument.createLayer({name: "myLayer", opacity: 80, blendMode: "colorDodge" }));
     }
 
-    async function getAllForms() {
-        return forms;
-        // if(forms.length !== 0) return forms;
-        // setForms([]);
-        // setForms(await getAllOfEntity('forms'));
-        // return forms;
+    async function fetchFormCategory(){
+        let pluginFolder = await uxp.storage.localFileSystem.getPluginFolder();
+        let folder = pluginFolder;
+        for(let p of currentFormFolder){
+            folder = await folder.getEntry(p)
+        }
+        let entries = await folder.getEntries();
+        let infoFile = entries.find(entry => entry.name.includes('.info'));
+        if(infoFile){
+            let title = infoFile.name.split('.info')[0];
+            let categoryItems = entries.filter(entry => entry.isFolder).map(folder => folder.name);
+            return {title: title, categoryItems: categoryItems };
+        } else{
+            let formItems = entries.filter(entry => entry.isFile).map(file => file.name.split('.')[0]);
+            return {title: null, categoryItems: null, formItems: formItems}
+        }
+        return null;
+    }
+
+    function prevCategory(){
+        currentFormFolder.pop();
+        fetchFormCategory().then(resolve => setFormCategory(resolve));
+    }
+    function nextCategory(title, item) {
+        currentFormFolder.push(item);
+        fetchFormCategory().then(resolve => setFormCategory(resolve));
     }
 
     async function getAllOfEntity(entities){
         let result = [];
         let pluginFolder = await uxp.storage.localFileSystem.getPluginFolder();
-        let formsFolder = await pluginFolder.getEntry("allFiles/" + entities);
-        let entries = await formsFolder.getEntries();
+        let folder = await pluginFolder.getEntry("allFiles/" + entities);
+        let entries = await folder.getEntries();
         for (const file of entries.filter(entry => entry.isFile)) {
             let bytes = await file.read({format: formats.binary})
             result.push(
@@ -82,47 +110,73 @@ export const ColorPicker = () => {
         return window.btoa( binary );
     }
 
+    function searchForms(e) {
+        if(e.target && e.target.value){
+            if(formCategory && formCategory.formItems){
+                setFilteredFormItems(formCategory.formItems.filter(item => item.includes(e.target.value.trim())))
+            }
+        } else {
+            setFilteredFormItems(formCategory.formItems);
+        }
+
+    }
+
+    function searchForms2(e) {
+        if(e.target && e.target.value){
+            if(formCategory && formCategory.formItems){
+                setFilteredFormItems(formCategory.formItems.filter(item => item.includes(e.target.value.trim())))
+            }
+        } else {
+            setFilteredFormItems(formCategory.formItems);
+        }
+
+    }
+
     return (
         <div>
             <div>
                 <h1>Форма</h1>
-                <h2>Тип формы</h2>
-                <sp-radio-group label="Medium" selected="man" name="Тип формы">
-                    <sp-radio value="military" size="m">Военная</sp-radio>
-                    <sp-radio value="mchs" size="m">МЧС</sp-radio>
-                </sp-radio-group>
-                <h2>Пол формы</h2>
-                <sp-radio-group label="Medium" selected="man" name="Пол формы">
-                    <sp-radio value="man" size="m">Мужская</sp-radio>
-                    <sp-radio value="woman" size="m">Женская</sp-radio>
-                </sp-radio-group>
-                <h2>Цвет формы</h2>
-                <sp-radio-group label="Medium" selected="man" name="Цвет формы">
-                    <sp-radio value="black" size="m">Черная</sp-radio>
-                    <sp-radio value="blue" size="m">Синяя</sp-radio>
-                    <sp-radio value="mchs" size="m">Мчс</sp-radio>
-                </sp-radio-group>
-                <h2>Просветы (окантовка)</h2>
-                <sp-radio-group label="Medium" selected="man" name="Просветы (окантовка)">
-                    <sp-radio value="black" size="m">Белая</sp-radio>
-                    <sp-radio value="blue" size="m">Синяя</sp-radio>
-                    <sp-radio value="mchs" size="m">Красная</sp-radio>
-                </sp-radio-group>
-            </div>
-            <sp-picker>
-                <sp-textfield
-                    id="searchFormsInput" type="search">
-                </sp-textfield>
-                <sp-menu>
-                    {forms.map((form, index) => {
+                {(() => {
+                    if(formCategory && formCategory.title != null && formCategory.categoryItems != null){
                         return (
-                            <sp-menu-item onClick={(e) => createNewLayerInApp(e)} className={'searchFormsBtn'} key={index}>
-                                {form.name}
-                            </sp-menu-item>
+                            <div>
+                                <h2>{formCategory.title}</h2>
+                                <sp-radio-group label="Medium" name='${formCategory.title}' >
+                                    {formCategory.categoryItems.map((itemName, index) => {
+                                        return (
+                                            <sp-radio onInput={() => nextCategory(formCategory.title, itemName)} value="${itemName + index}" size="m" key={itemName + index}>{itemName}</sp-radio>
+                                        )
+                                    })}
+                                </sp-radio-group>
+                            </div>
                         )
-                    })}
-                </sp-menu>
-            </sp-picker>
+                    }
+                })()}
+            </div>
+            {(() => {
+                if(formCategory && formCategory.formItems){
+                    return (
+                        <div>
+                            <h2>Список форм</h2>
+                            <sp-picker onClick={(e) => searchForms2(e)}>
+                                <input onChange={(e) => searchForms(e)}
+                                       id="searchFormsInput" type="search">
+                                </input>
+                                <sp-menu>
+                                    {filteredFormItems.map((formName, index) => {
+                                        return (
+                                            <sp-menu-item onClick={(e) => createNewLayerInApp(e)} className={'searchFormsBtn'} key={index}>
+                                                {formName}
+                                            </sp-menu-item>
+                                        )
+                                    })}
+                                </sp-menu>
+                            </sp-picker>
+                        </div>
+                    )
+                }
+            })()}
+            {currentFormFolder.length > 2 ? <sp-button id="prevCategoryBtn" onClick={prevCategory}>Назад</sp-button> : null}
         </div>
         );
     }
